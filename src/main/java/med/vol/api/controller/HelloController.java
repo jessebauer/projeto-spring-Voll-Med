f92ef1c2ac4df22ab38e92b4a9d115a51b4f22e5
Jesse Muehlbauer <jesse_ruan@hotmail.com>
2023-02-16 16:05:57 -0300

    Post de cadastro medito padrao DTO

--- src/main/java/med/vol/api/controller/HelloController.java
+++ src/main/java/med/vol/api/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    public String OlaMundo(){
+    public String OlaMundo() {
         return "Hello World Spring!";
     }
 }
